33051c9a372752dae2b5df6b32849d311cb38594
cedcoss@nkit <ankitaginhotry@cedcoss.com>
2022-02-11 13:15:17 +0530

    Add to Cart Feature added

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
+//Globle array and var.
 var products = [
     { "id": 101, "name": "Basket Ball", "image": "basketball.png", "price": 150 },
     { "id": 102, "name": "Football", "image": "football.png", "price": 120 },
     { "id": 103, "name": "Soccer", "image": "soccer.png", "price": 110 },
     { "id": 104, "name": "Table Tennis", "image": "table-tennis.png", "price": 130 },
     { "id": 105, "name": "Tennis", "image": "tennis.png", "price": 100 }];
-    
+
+var cartArr=[];
+var obj={};
+
+//dislay()--display all iteam of products[] raay;
 function Display(){
     var lst="";
     for(var i=0;i<products.length;i++){
@@ -13,10 +18,60 @@ function Display(){
         <li><h3>ID</h3>"+products[i].id+"</li>\
         <li><h3>Name :</h3>"+products[i].name+"</li>\
         <li><h3>Price</h3>"+products[i].price+"$</li>\
-        <li><button>Add to cart</button></li>\
+        <li><button onclick='addTOcart("+products[i].id+")'>Add to cart</button></li>\
         </div>"
     }
     $("#main").html(lst);
+}
+
+
+//add to cart
+function addTOcart(id){
+    console.log(' Add to cart working');
+    for(var i=0;i<products.length;i++){
+        if(id==products[i].id){
+            obj[i]={
+                pid:String(products[i].id),
+                pname:products[i].name,
+                pimage:products[i].image,
+                pprice:String(products[i].price),
+            };
+           
+            console.log(obj[i]);
+            cartArr.push(obj[i]);
+           
+
+
+        }
+        
+    }
+    displayCart();
+    
+    //checking cartarray
+    // for(var j=0;j<cartArr.length;j++){
+    //     console.log("array: "+cartArr[j].pname);
+    // }
+
+    
+
+    //checing json obj
+}
+
+//diaplayCart Item
+
+function displayCart(){
+    var temp="";
+    for(var i=0;i<cartArr.length;i++){
+        temp+="<tr>\
+        <td><img src='/images/"+cartArr[i].pimage+"'></td>\
+        <td>"+cartArr[i].pid+"</td>\
+        <td>"+cartArr[i].pname+"</td>\
+        <td>"+cartArr[i].pprice+"$</td>\
+        <td>1</td>\
+        <td><buttton>❌</buttton></td>\
+        </tr>"
+    }
+    $('#tBody').html(temp);
 }
 
 $(document).ready(function () {
